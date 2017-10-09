1f9371d2a5802c25405f759cf1412cbe733bc095
Johans <2574580419@qq.com>
2017-10-09 11:50:20 +0000

    Add a Savenger prototype for collecting failed jobs automaticlly

---
+++ Util/Savenger.php
@@ -0,0 +1,21 @@
+<?php
+namespace Util;
+/**
+ * @class Savenger 
+ * Collect some failure jobs just do  what savengers do
+ */
+
+class Savenger {
+    
+    private $list = [];
+
+    /**
+     * @function  collect
+     *
+     * collect the failed jobs
+     *
+     */
+    public function collect() {
+    }
+}
+
