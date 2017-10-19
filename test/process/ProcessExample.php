385657b0d6fdf7761e4e58d638c2b47b0f39bc5a
Johans <2574580419@qq.com>
2017-10-19 20:05:42 +0800

    Improve ProcessExample test

--- test/process/ProcessExample.php
+++ test/process/ProcessExample.php
@@ -14,33 +14,46 @@ for($i = 1; $i<= $task; $i++) {
     }
     if ($pid == 0) {
         $current_pid = getmypid();
-        $process_pool = @unserialize(shmop_read($process_pool_shm,0,1024));
+        $process_pool_size = shmop_size($process_pool_shm);
+        $process_pool = @unserialize(shmop_read($process_pool_shm,0,$process_pool_size));
         //store child process data into specific memory
         $child_data  = array('pid'=>$current_pid,"data"=>[rand(),'hello']);
         $child_key = ftok(__FILE__,chr($current_pid));
-        $child_shm = shmop_open($child_key,'c',0644,1024);
+        $size = 1024*1024; 
+        $child_shm = shmop_open($child_key,'c',0644,$size);
         shmop_write($child_shm,serialize($child_data),0);
-        sleep(20);
         exit(0);
     } else {
        $process_pool[$pid] = array($pid);
        shmop_write($process_pool_shm,serialize($process_pool),0);
-
    }
 }
 
-if (pcntl_waitpid(-1,$status)) {
-    //Read data from all child process
-    foreach($process_pool as $pid => $pid_info) {
-        $tmp_key = ftok(__FILE__,chr($pid));
-        $tmp_shm = shmop_open($tmp_key,'c',0644,1024);
-        $child_data = @unserialize(shmop_read($tmp_shm,0,1024));
-        shmop_delete($tmp_shm);
-        if (!empty($child_data)) {
-            $datas[$child_data['pid']] = $child_data;
-        }
+while(pcntl_waitpid(-1,$status) > 0);
+//Read data from all child process
+foreach($process_pool as $pid => $pid_info) {
+    $tmp_key = ftok(__FILE__,chr($pid));
+    $size= 1024*1024;;
+    $tmp_shm  = shmop_open($tmp_key,'a',0644,$size);
+    $org_data = shmop_read($tmp_shm,0,$size);
+    $child_data = @unserialize($org_data);
+    if (empty($child_data)) {
+        echo "$tmp_key\n";
+    }
+    shmop_delete($tmp_shm);
+    shmop_close($tmp_shm);
+    if (!empty($child_data)) {
+        $datas[$pid] = $child_data;
+    }
 }
-    var_dump($datas);
-    shmop_delete($process_pool_shm);
+var_dump(count($datas));
+var_dump(count($process_pool));
+var_dump(count(array_keys($datas)));
+var_dump(count(array_keys($process_pool)));
+foreach (array_keys($process_pool) as $p_key) {
+    if (!in_array($p_key,array_keys($datas))) {
+        echo $p_key."\n";
+    }
 }
-
+shmop_delete($process_pool_shm);
+shmop_close($process_pool_shm);
