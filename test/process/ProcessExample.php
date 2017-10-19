0d2f20c3d4bfb69cb3411e763277a5ac4b1b553d
johans <2574580419@qq.com>
2017-10-19 10:37:01 +0000

    Continue to perfect ProcessExample for shared memory problem

--- test/process/ProcessExample.php
+++ test/process/ProcessExample.php
@@ -1,27 +1,26 @@
 <?php
 //limit the task to be forked
-$task = 10;
+$task = 100;
 $process_pool = array();
 //allocate 1kb memory segment to store process_pool
 $process_pool_key = ftok(__FILE__,chr(0));
 $process_pool_shm = shmop_open($process_pool_key,'c',0644,1024);
 $datas = array();
 
-for($i = 1; $i<= 10; $i++) {
+for($i = 1; $i<= $task; $i++) {
     $pid = pcntl_fork();
     if ($pid == -1) {
         die("Can't fork child process.");
     }
     if ($pid == 0) {
         $current_pid = getmypid();
-        $process_pool = unserialize(shmop_read($process_pool_shm,0,1024));
+        $process_pool = @unserialize(shmop_read($process_pool_shm,0,1024));
         //store child process data into specific memory
         $child_data  = array('pid'=>$current_pid,"data"=>[rand(),'hello']);
-        var_dump($child_data);
         $child_key = ftok(__FILE__,chr($current_pid));
         $child_shm = shmop_open($child_key,'c',0644,1024);
         shmop_write($child_shm,serialize($child_data),0);
-        sleep(30);
+        sleep(20);
         exit(0);
     } else {
        $process_pool[$pid] = array($pid);
@@ -42,7 +41,6 @@ if (pcntl_waitpid(-1,$status)) {
         }
 }
     var_dump($datas);
-    var_dump($process_pool);
     shmop_delete($process_pool_shm);
 }
 
