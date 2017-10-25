083aeadb04c8ad960c6eb97a06e18bdaad970d2f
johans <2574580419@qq.com>
2017-10-25 10:58:30 +0000

    Add module for process

---
+++ Module/Process/Process.php
@@ -0,0 +1,10 @@
+<?php
+namespace Module\Process;
+/**
+ * @class Process
+ * This function is a simple wrapper of pcntl_fork for PHP multi-process supportant
+ */
+class Process {
+    public function __construct(){
+    }
+}
