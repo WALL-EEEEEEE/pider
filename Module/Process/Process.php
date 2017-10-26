371a38e152423c9ecc5092a0c5b1a44ee3d7674d
Johans <2574580419@qq.com>
2017-10-26 20:35:05 +0800

    Add Process model

---
+++ Module/Process/Process.php
@@ -0,0 +1,17 @@
+<?php
+namespace Module\Process;;
+
+class ProcessPool {
+
+   private $process_pool = array();
+
+   public function add(){
+   }
+   public function write(){
+
+   }
+   public function read(){
+
+   }
+}
+
