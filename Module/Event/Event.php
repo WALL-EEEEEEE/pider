3052a0af0a3864406854356419d2150c3a0bfdca
johans <2574580419@qq.com>
2017-11-10 19:56:25 +0000

    Multiple process model

--- Module/Event/Event.php
+++ Module/Event/Event.php
@@ -1,3 +1,7 @@
 <?php
 
+class Event {
+
+}
+
 
---
+++ Module/Process/Processd.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @class Processd 
+ * 
+ * Manage several processes as a process manager
+ *
+ */
+
+class Processd {
+    /**
+     * @member  $process_pool 
+     * Hold all the processes
+     */
+    private $process_pool = [];
+    /**
+     *@method add(callback | Process $process )  attach a process to manager
+     *@param $process mix $process can be only a callback or a Process object
+     *@return void
+     */
+    public function add($process) {
+    }
+    /**
+     * @method shared(Shared $shared) shared data that is accessible for both parent process and child process.
+     *
+     * @param $shared Shared  Data shared
+     */
+    public function shared(Shared $shared) {
+    }
+
+    /**
+     * @method go() start to shedule and manager process
+     * @return void
+     */
+    public function go() {
+        $schedule = new Schedule($this->process_pool);
+        while($process = $schedule->shedule()) {
+            $process->run();
+        }
+    }
+}
