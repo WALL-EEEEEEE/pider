871f02d2cd08b7dad7a2ca87e42e5fca59a20e34
Johans <2574580419@qq.com>
2017-12-06 17:51:14 +0800

    --allow-empty-messages

--- Module/Http/Request.php
+++ Module/Http/Request.php
@@ -6,12 +6,27 @@ use GuzzleHttp\Client  as Client;
 
 class Request {
 
-    private $client = '';
+    private $proxy;
+    private static $proxy_handler;
+    private $client;
     public function __construct(array $config = []) {
         $this->client = new Client($config);
     }
-
+    public static function proxy_handler(callable $proxy_handler) {
+        self::$proxy_handler = $proxy_handler;
+    }
+    public function proxy(string $proxy) {
+        $this->proxy = $proxy;
+    }
     public function request($method, $uri = '', array $options = []) {
+        if (!empty(self::$proxy_handler)) {
+            $proxy_handler = self::$proxy_handler;
+            $options['proxy'] =  $proxy_handler();
+        }
+        if (!empty($proxy)) {
+            $options['proxy'] = $this->proxy;
+        }
+        var_dump($options);
         return new Response($this->client->request($method,$uri, $options));
     }
 
