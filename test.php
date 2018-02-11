f3b14569a9d6e4c5518f05c74e7978a5efdf82e3
johans <2574580419@qq.com>
2018-02-11 05:59:50 +0000

    Add spider to crawle pictures for JdWineLinkConsistentCensorPictures

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+DB::table('users')
+            ->whereExists(function ($query) {
+                $query->select(DB::raw(1))
+                      ->from('orders')
+                      ->whereRaw('orders.user_id = users.id');
+            })
+            ->get();
+
+
