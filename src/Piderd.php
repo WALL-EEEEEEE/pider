7b00598d82444e1c0870fef780361bb18f361f73
johans <duanqiaobb@163.com>
2018-06-04 13:54:01 +0000

    --allow-empty-message

---
+++ src/Piderd.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+require_once('vendor/autoload.php');
+require_once('Extension/autoloader.php');
+date_default_timezone_set('Asia/Shanghai');
+defined('FRAMEWORK_NAME')?'':define('FRAMEWORK_NAME','Pider');
+defined('APP_ROOT')?'':define('APP_ROOT',dirname(__DIR__,1));
+defined('PIDER_PATH')?'':define('PIDER_PATH',dirname(__FILE__));
+Pider\Extension\autoloader::autoload();
